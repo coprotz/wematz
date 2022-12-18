f120b38ed689926de7221d27b30deb5a2c58400e
barruTech <tipwindi26@gmail.com>
2022-12-18 11:40:33 +0300

    iuiu

--- src/pages/health/Doctors.jsx
+++ src/pages/health/Doctors.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
-// import { doctors } from '../../data'
-import {  BsArrowLeft } from "react-icons/bs";
 import { useNavigate } from 'react-router-dom';
 import Search from '../../components/search/Search';
 import DoctorCard from './DoctorCard';
 import useData from '../../hooks/useData';
 import { useState } from 'react';
 import Appointment from '../appointments/Appointment';
+import { HiOutlineArrowLeft } from 'react-icons/hi'
 
 
 const Doctors = () => {
@@ -18,9 +17,16 @@ const Doctors = () => {
     <div className='doctors_wrappers'>
         {appoint && <Appointment doctor={appoint} setAppoint={setAppoint}/>}
         <div className="doctors_top">
+          <div className="heading_top">                
+                <div className="view_que_back">
+                  <button onClick={() =>navigate(-1)} className='btn_btn'><HiOutlineArrowLeft/></button>
+                </div>
+                <h2 className='title'>Madaktari</h2>           
+              </div>
             <h1>Karibu kwenye ukurasa watu wa WemaDocta, Madaktari wapo tayari kukusikiliza na kukushauri!!!</h1>
-            <div>
-              <button className='btn_sign' onClick={() => navigate('/health')} >Tazama Clinics</button>  
+            <div className='clinics_btns'>
+              <button className='btn_sign' onClick={() => navigate('/health')} >Kliniki</button>  
+              <button className='btn_sign' onClick={() => navigate('/health')} style={{backgroundColor: 'white', border: '1px solid blue', color:'blue'}}>Makala</button>  
             </div>
             
         </div>
--- src/pages/health/MainHealth.jsx
+++ src/pages/health/MainHealth.jsx
@@ -7,6 +7,7 @@ import { FaHeartbeat } from "react-icons/fa";
 import ClinicCard from './ClinicCard';
 import Footer from '../../components/footer/Footer';
 import useData from '../../hooks/useData';
+import { HiOutlineArrowLeft } from 'react-icons/hi'
 
 
 const MainHealth = () => {
@@ -16,11 +17,20 @@ const MainHealth = () => {
     <div className='main_health'>
         <div className="health_top">
             <div className="health_t_1">
-                <h1>Karibu katika Ukumbi wa Afya na Ushauri wa Kitabibu</h1>
-                <span className='health_p'>Shauriana na Daktari juu ya masuala yako ya Afya, 
-                    weka appointment na daktari wa shida yako kutoka katika listi ya madaktari tulionao.
-                </span>
-                <button className='btn_sign' onClick={() =>navigate('/health/doctors')}>Angalia Madaktari</button>
+              <div className="heading_top">                
+                <div className="view_que_back">
+                  <button onClick={() =>navigate(-1)} className='btn_btn'><HiOutlineArrowLeft/></button>
+                </div>
+                <h2 className='title'>Afya</h2>           
+              </div>
+              <h1>Karibu katika Ukumbi wa Afya na Ushauri wa Kitabibu</h1>
+              <span className='health_p'>Shauriana na Daktari juu ya masuala yako ya Afya, 
+                weka appointment na daktari wa shida yako kutoka katika listi ya madaktari tulionao.
+              </span>     
+              <div className='clinics_btns'>
+                <button className='btn_sign' onClick={() => navigate('/health/doctors')} >Madaktari</button>  
+                <button className='btn_sign' onClick={() => navigate('/health/makala')} style={{backgroundColor: 'white', border: '1px solid blue', color:'blue'}}>Makala</button>  
+              </div>
             </div>
             <div className="health_logo">
                 <FaHeartbeat/>
