8aaa0019d0de548eab2cc9c8fb3dfdb955427b13
barruTech <tipwindi26@gmail.com>
2022-12-21 15:13:40 +0300

    jkjh

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,14 +1,25 @@
+import { doc, updateDoc } from 'firebase/firestore'
 import React from 'react'
+import { useState } from 'react'
+import { BsThreeDotsVertical } from 'react-icons/bs'
 import { useNavigate } from 'react-router-dom'
 import logo from '../../assets/images/logo_512.png'
-import { useAuth } from '../../hooks/useAuth'
+import { db, useAuth } from '../../hooks/useAuth'
 import useData from '../../hooks/useData'
+
 
 const Nav = () => {
     const navigate = useNavigate()
-    const { user } = useAuth()
+    const { user, logOut } = useAuth()
     const { users } = useData()
     const cuUser = users?.find(u => u.id === user?.uid)
+    const [show, setShow] = useState(null)
+
+    const handleLogout = async () => {
+        await updateDoc(doc(db, 'users', `${cuUser?.id}`), { isOnline: false})
+        await logOut()
+      
+    }
   return (
     <div className="register_nav">
         <div className="logo" style={{width:'90px', height: '90px'}} onClick={() => navigate('/')}>
@@ -21,9 +32,24 @@ const Nav = () => {
             <span onClick={() =>navigate('/contacts')}>Mawasiliano</span>
         </div>
         {cuUser ? 
-        
-        <div className="profile_img" onClick={() => navigate('/')}>
-            <img src={cuUser?.photo ? cuUser?.photo : process.env.PUBLIC_URL + cuUser?.avatar} />
+        <div className='nav_actions'>        
+            <div className="profile_img" onClick={() => navigate('/')}>
+                <img src={cuUser?.photo ? cuUser?.photo : process.env.PUBLIC_URL + cuUser?.avatar} />
+            </div>
+            <div className="user_action" onMouseEnter={() =>setShow(true)} onMouseLeave={() =>setShow(false)}>
+                <button className='btn_btn' ><BsThreeDotsVertical/></button>
+                {show &&
+                <div className="user_menu">
+                    <span onClick={() => {navigate('/');setShow(null)}}>Nyumbani</span>
+                    <span onClick={() => {navigate('/mjaheed');setShow(null)}}>Kuwa Mjaheed</span> 
+                    <span onClick={() => {navigate('/myAccounts');setShow(null)}}>Akaunti Zangu</span> 
+                    {/* {isMarry &&
+                    <span onClick={() => {navigate(`/nikah/${marry?.id}`);setShow(null)}}>Wasifu wa Nikah</span>
+                    } */}
+                    <span onClick={() => {navigate('/subscriptions');setShow(null)}}>Unga Mkono</span>                      
+                    <span onClick={handleLogout}>ONDOKA</span>
+                </div>}
+            </div>
         </div>
         :
     
